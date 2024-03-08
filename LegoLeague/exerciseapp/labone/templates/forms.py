2d3c6c72d58a2e2924b5a1d9f74c9973eec097c0
abulhasan4 <121133179+abulhasan4@users.noreply.github.com>
2024-03-08 22:57:06 +0000

    Add files via upload

---
+++ LegoLeague/exerciseapp/labone/templates/forms.py
@@ -0,0 +1,22 @@
+from .models import *
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.forms import ModelForm, ModelChoiceField
+from django.db import transaction
+
+class UserSignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_admin = False
+        user.email = self.cleaned_data['username']
+        user.save()
+        return user
+
+
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
---
+++ LegoLeague/exerciseapp/labone/templates/views.py
@@ -0,0 +1,39 @@
+from django.http import HttpResponse
+import re
+from django.shortcuts import render, get_object_or_404
+import random
+from .models import *
+from .forms import *
+from django.views.generic import CreateView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+
+def index(request):
+    return render(request, 'index.html')
+
+class UserSignupView(CreateView):
+    model = User
+    form_class = UserSignupForm
+    template_name = 'register.html'
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('dashboard')
+
+class UserLoginView(LoginView):
+    template_name='login.html'
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("/")
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
